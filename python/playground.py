787f7ff2a64a9d46e180c19d0e116cbc027d190e
Magnus Friberg <mfriberg@spotify.com>
2020-10-09 20:27:37 +0200

    Decorators

---
+++ python/playground.py
@@ -0,0 +1,43 @@
+"""
+Subject: Decorators
+Date: 2020-10-08
+"""
+import time
+
+
+def decorator_function(some_function):
+
+    def wrapper_function(*args, **kwargs):
+        """Anything in this code witl be runned together with some_function.
+
+        Arguments: Add *args and **kwargs to allow arbitrtary input to
+                   some_function.
+        """
+        # Start counting
+        start = time.time()
+        # Run method
+        result = some_function(*args, **kwargs)
+        # Stop counting
+        end = time.time()
+        print('Time elapsed: {}'.format(end - start))
+        # Retturn input function result
+        return result
+
+    return wrapper_function
+
+
+# Wrappoing decorator
+@decorator_function
+def say_hello():
+    print('Hi Magnus')
+
+
+# Wrappoing decorator
+@decorator_function
+def say_hello_with_args(name):
+    print('Hi {}'.format(name))
+
+
+# Execute functions
+say_hello()
+say_hello_with_args('Magnus Friberg')
